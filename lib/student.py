39e74844afcbd5dbc90f38a62f8c3e7fc271268d
kevin <kipronok234@gmail.com>
2024-09-03 13:17:54 +0300

    python-p3-inheritance-lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,15 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = []  # Initialize 'knowledge' as an empty list
+
+
     
-    def learn(self):
+    
+    
+    def learn(self,knowledge_str):
+        self.knowledge.append(knowledge_str)
         pass
